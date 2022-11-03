3adac4a92cc78afa3a1ead36b438e6ddf6b60258
Ezra Chung <ezra.chung97@gmail.com>
2022-11-03 16:13:26 -0700

    Completed put request. Working on delete request

--- db.js
+++ db.js
@@ -3,10 +3,10 @@
 const { Client } = require("pg");
 
 const DB_URI = process.env.NODE_ENV === "test"
-  // ? "postgresql://ezray:secret@localhost/biztime_test"
-  // : "postgresql://ezray:secret@localhost/biztime";
-  ? "postgresql:///biztime_test"
-  : "postgresql:///biztime";
+  ? "postgresql://ezray:secret@localhost/biztime_test"
+  : "postgresql://ezray:secret@localhost/biztime";
+  // ? "postgresql:///biztime_test"
+  // : "postgresql:///biztime";
 
 let db = new Client({
   connectionString: DB_URI
--- routes/companies.js
+++ routes/companies.js
@@ -57,4 +57,52 @@ router.post('/', async function (req, res, next) {
   return res.status(201).json({ company });
 });
 
+
+/** Make a PUT request. Edit existing company */
+router.put("/:code", async function(req,res,next){
+  debugger;
+  if (req.body === undefined) throw new BadRequestError();
+
+  const { name, description } = req.body;
+  const code = req.params.code;
+
+  if(!name || !description) {
+    throw new BadRequestError(
+    "Invalid Input: Please input name and description"
+  )};
+
+  const result = await db.query(
+      `UPDATE companies
+        SET name = $1,
+            description = $2
+        WHERE code = $3
+        RETURNING code, name, description`, [name, description, code]);
+
+  const company = result.rows[0];
+
+  if(company === undefined) {
+    throw new NotFoundError();
+  }
+
+  return res.json({company});
+})
+
+/** Makes DELETE request. Deletes company and returns JSON {status:"deleted"} 
+ * If company does not exist, returns 404 error
+*/
+
+router.delete("/companies/:code", async function(req, res, next){
+  debugger;
+  const result = await db.query(
+    `DELETE FROM companies 
+      WHERE code = $1
+      RETURNING code, name, description`, [req.params.code]
+  );
+
+  // debugger;
+  if(result.rows.length === 0) throw new NotFoundError();
+
+  return res.json({status:"deleted"});
+});
+
 module.exports = router;
