8bef03ca4b0b8df0788a5bb520f017ee8f605343
clchen-arcadia <clchen.arcadia@gmail.com>
2022-11-03 15:09:20 -0400

    Add get /companies route

--- routes/companies.js
+++ routes/companies.js
@@ -8,9 +8,13 @@ const db = require('../db');
 const { BadRequestError, NotFoundError } = require("../expressError");
 
 
-router.get('/', function (req, res) {
+router.get('/', async function (req, res, next) {
 
-  
+  const results = await db.query(
+    `SELECT code, name
+      FROM companies`);
+
+  const companies = results.rows;
 
   return res.json({ companies });
 });
